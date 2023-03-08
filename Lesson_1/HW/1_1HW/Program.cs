0259aeb9faee4057ab729dc28cb2288265ecd4a2
Daria_lev <375293252007@yandex.by>
2023-03-08 20:45:12 +0300

    task2

---
+++ Lesson_1/HW/1_1HW/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write a number max: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max=0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);
