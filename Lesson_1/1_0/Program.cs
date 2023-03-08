b614620183043088e9d774e32a43b505b3922915
Daria_lev <375293252007@yandex.by>
2023-03-08 11:42:15 +0300

    task 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write a number: ");
+   
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
