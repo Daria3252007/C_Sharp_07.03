e6e11dbe61a1e750085d737a1e336d18265c5dcc
Daria_lev <375293252007@yandex.by>
2023-03-08 20:46:22 +0300

    task1

--- Lesson_1/HW/1_0HW/Program.cs
+++ Lesson_1/HW/1_0HW/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Write a number max: ");
-int 
+Console.WriteLine("Write a number max and min: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine("max = a min = b");   
+}
+else if (b > a)
+{
+    Console.WriteLine("max = b min = a");
+}
+
+
