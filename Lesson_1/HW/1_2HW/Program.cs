c453a8e7be40e79872dc49442db2b3daa793d80b
Daria_lev <375293252007@yandex.by>
2023-03-08 21:36:29 +0300

    task3

---
+++ Lesson_1/HW/1_2HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write if the number is even: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
