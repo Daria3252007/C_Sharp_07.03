006549d5bee9725731b1d5167bd8e186f370b9c7
Daria_lev <375293252007@yandex.by>
2023-03-08 12:43:05 +0300

    task1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a namber:" );
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a == b * b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
