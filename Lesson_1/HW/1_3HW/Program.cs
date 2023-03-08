4f23d000839fd3bba30923746afe5687b165a47d
Daria_lev <375293252007@yandex.by>
2023-03-08 21:37:51 +0300

    task4

---
+++ Lesson_1/HW/1_3HW/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write all even numbers from 1 to N");
+int N = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= N; i++)
+{
+    if(i %2 ==0)
+    {
+        Console.WriteLine(i);
+    }
+}
