afdd10281ea6b652354fe2f330858919c3b40ec3
Daria_lev <375293252007@yandex.by>
2023-03-13 21:39:05 +0300

    task 2_0

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int TekeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+// 123 % 10 -> 3
+// 123 // 10 -> 12
+
+int th_num = new Random().Next(100, 1000);
+int result = TekeNum(th_num);
+Console.WriteLine(result);
