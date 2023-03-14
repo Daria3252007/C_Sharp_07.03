3938de6e63f1331214a813ee5beec21606500f12
Daria_lev <375293252007@yandex.by>
2023-03-14 21:54:09 +0300

    task 2_1

---
+++ Lesson_2/HW2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int a(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 100;
+    int num2 = num % 10;
+    int res = num1 * 10 + num2;
+    return res;
+}
+int b = a(new Random().Next(100,1000));
+Console.WriteLine(b);
