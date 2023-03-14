05cac296e37c0adf02e590f603b5f22aef21b286
Daria_lev <375293252007@yandex.by>
2023-03-14 19:20:18 +0300

    task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿void Del(int a)
+{
+    Console.WriteLine(a);
+    if (a%7==0 & a%23==0) 
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+    int random = new Random().Next(1, 1000);
+    Del(random);
+    
