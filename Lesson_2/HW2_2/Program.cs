077fc786607c6c92457be57684bfe6f24952c851
Daria_lev <375293252007@yandex.by>
2023-03-15 22:36:13 +0300

    task 2_2

---
+++ Lesson_2/HW2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿void t (int num)
+{
+    Console.WriteLine(num);
+    if (num < 100)
+    {
+        Console.WriteLine("Tретьей цифры нет");
+    }
+    else
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        num = num % 10;
+        Console.WriteLine(num);
+    }
+}
+int r = new Random().Next(10,10000);
+t(r);
