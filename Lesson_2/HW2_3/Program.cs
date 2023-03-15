c858fdbc2f01093276803ac25bc571224b32c3ea
Daria_lev <375293252007@yandex.by>
2023-03-15 22:06:28 +0300

    task 2_3

---
+++ Lesson_2/HW2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+void day (int a)
+{
+    if (a > 5)
+    {
+        Console.WriteLine("Yes");
+    }
+    else 
+    {
+        Console.WriteLine("No");
+    }
+}
+day(a);
