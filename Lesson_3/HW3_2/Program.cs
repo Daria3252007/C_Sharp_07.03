ff463f3c9b296a61d9841635cc1867f7aae24de6
Daria_lev <375293252007@yandex.by>
2023-03-16 17:31:47 +0300

    task 3_2

---
+++ Lesson_3/HW3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int num)
+{
+    int c = 1;
+    while (c <= num)
+    {
+        Console.WriteLine(Math.Pow(c,3));
+        c++;
+    }
+}
+Console.WriteLine("Число");
+Cube(int.Parse(Console.ReadLine()!));
