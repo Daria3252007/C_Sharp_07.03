4ae8661f76884360b0e8ddee1a26807730c110e8
Daria_lev <375293252007@yandex.by>
2023-03-19 13:34:06 +0300

    task 4_0

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,11 @@
+﻿int SunNum(int num)
+{
+    int all_sum = 0;
+
+    for (int i = 0; i <= num; i++)
+        all_sum += i;
+    return all_sum;
+}
+int val = int.Parse(Console.ReadLine()!);
+int result = SunNum(val);
+Console.WriteLine(result);
