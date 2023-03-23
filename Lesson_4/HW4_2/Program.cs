0b83e7c695f69f23c2edb78991fc2081bc3a3598
Daria_lev <375293252007@yandex.by>
2023-03-23 17:06:24 +0300

    task 4_2

---
+++ Lesson_4/HW4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int m(int l, int a, int b)
+{
+    int[] arr = new int[l];
+    for (int i = 1; i <= l; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+    return arr;
+}
+Console.WriteLine("Длина:");
+int l = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Начало:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Конец:");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(m);
