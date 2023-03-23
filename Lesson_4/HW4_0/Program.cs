69acf47f1f73fbb10d24b2e5f57ae2ffb7f0f185
Daria_lev <375293252007@yandex.by>
2023-03-23 17:06:07 +0300

    task 4_0

---
+++ Lesson_4/HW4_0/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int n(int a, int b)
+{
+    int x = 1;
+    for (int i = 0; i <= b; i++)
+    {
+        x *= a;
+    }
+    return x;
+}
+Console.WriteLine("A:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("B:");
+int b = int.Parse(Console.ReadLine()!);
+int r = int.Parse(Console.ReadLine()!);
+n(r);
+
