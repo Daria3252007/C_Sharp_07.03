018f57a3598395c8cb58e58e7bc00f36caab686d
Daria_lev <375293252007@yandex.by>
2023-03-29 15:22:44 +0300

    task 5_2

---
+++ Lesson_5/HW5_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void PrintArr(double[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.WriteLine();
+}
+double[] RandArr()
+{
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    double[] arr = new double[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+        arr[i] = rnd.Next(-100, 100) + rnd.NextDouble();
+    for (int i = 0; i < len; i++)
+        Console.WriteLine($"{arr[i]}");
+    return arr;
+}
+double MinMax(double[] arr)
+{
+    double min =arr[0];
+    double max =arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]<min)
+        min=arr[i];
+        if (arr[i]>max)
+        max=arr[i];
+    }
+    double diff= max- min;
+    return diff;
+}
+
+
+double[] mass = RandArr();
+double res=MinMax(mass);
+Console.Write(res);
+
