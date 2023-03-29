d81bf64ff5a08a425e9a4588b17f46bff6a036ef
Daria_lev <375293252007@yandex.by>
2023-03-29 13:18:04 +0300

    task 5_0

---
+++ Lesson_5/HW5_0/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void PrintArr(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.WriteLine();
+}
+int[] RandArr()
+{
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    int min = 100;
+    int max = 999;
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+        arr[i] = new Random().Next(min, max + 1);
+    return arr;
+}
+int Even(int[] mas)
+{
+    int n = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] % 2 == 0)
+        {
+            n += 1;
+        }
+    }
+    return n;
+}
+int[] mass = RandArr();
+Console.WriteLine(Even(mass));
