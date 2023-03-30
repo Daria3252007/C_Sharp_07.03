4fa11d6611cc35836ffb23c2a77d54c3da479ee1
Daria_lev <375293252007@yandex.by>
2023-03-30 21:08:24 +0300

    task 6_0

---
+++ Lesson_6/6_0/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+void PrintArr(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}   ");
+    Console.WriteLine();
+}
+int[] RandArr()
+{
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+        arr[i] = new Random().Next(min, max + 1);
+    return arr;
+}
+void Rev(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size / 2; i++)
+        (arr[i], arr[size - i - 1]) = (arr[size - i - 1],arr[i]);
+}
+
+int[] mass = RandArr();
+PrintArr(mass);
+Rev(mass);
+PrintArr(mass);
