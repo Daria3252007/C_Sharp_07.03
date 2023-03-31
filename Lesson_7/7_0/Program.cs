90acd1a3c0b55ccc1c58464a763b6b46e890e392
Daria_lev <375293252007@yandex.by>
2023-03-31 15:39:34 +0300

    task 7_0

---
+++ Lesson_7/7_0/Program.cs
@@ -0,0 +1,42 @@
+﻿// 0. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums()
+{
+    Console.Write("Enter the number of rows: ");
+    int row = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter the number of columns: ");
+    int column = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter the min number of massive ");
+    int start = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter the max number of massive ");
+    int stop = int.Parse(Console.ReadLine()!);
+
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(start, stop + 1);
+
+    return arr;
+}
+int[,] mass = MassNums();
+Print(mass);
+
+
+
+
