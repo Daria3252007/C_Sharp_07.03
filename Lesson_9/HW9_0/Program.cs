9f8d60f79bcf619890ffd314831c08a3c16c5532
Daria_lev <375293252007@yandex.by>
2023-04-10 15:31:09 +0300

    task 9_0

---
+++ Lesson_9/HW9_0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Enter number N: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number M: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+
+void Even(int N, int M)
+{
+    if (M% 2 ==0)
+    if (N <= M) return;
+    Even(N, M += 2);
+    Console.WriteLine($"{M} ");
+}
+Even(num,num2);
