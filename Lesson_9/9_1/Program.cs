002668d4543f77cdf7c46ea9fb83ac4c6d1869b5
Daria_lev <375293252007@yandex.by>
2023-04-10 11:04:53 +0300

    task 9_1

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Enter number N: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number M: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+void NaturalArr(int N, int M)
+{
+    if (M>N) return;
+    NaturalArr(M,N - 1);
+    Console.Write($"{N} ");
+}
+NaturalArr(num,num2);
